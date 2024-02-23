1918cc3ad17f5b4785343c096994ed2973ef76fe
OluwatosinOyagha <oluwatosin.mohammed@gmail.com>
2024-02-23 11:17:31 +0100

    Added Lesson 35 Task: My Personal Library

---
+++ Lesson 35 Task/app.js
@@ -0,0 +1,7 @@
+const books = {
+    title: 'This Present Darkness',
+    description: 'Gospel book illustrating angels and demons',
+    numberOfPages: 419,
+    author: 'Frank Peretti',
+    reading: true
+}
