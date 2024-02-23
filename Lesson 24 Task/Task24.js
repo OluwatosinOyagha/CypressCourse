4d106db3f22b10b21014b3259d4a7c3929ed75bd
OluwatosinOyagha <oluwatosin.mohammed@gmail.com>
2024-02-23 10:38:52 +0100

    Added Lesson 24 Task: Area of Rectangle 2

---
+++ Lesson 24 Task/Task24.js
@@ -0,0 +1,10 @@
+function multiply(length){
+    // multiply length and width return the equivalent dollar value
+    // conversion rate: 1800 naira to one dollar
+    const width = length * 400;
+    return width;
+}
+
+const areaOfRectangle = multiply(10)
+
+console.log(areaOfRectangle);
